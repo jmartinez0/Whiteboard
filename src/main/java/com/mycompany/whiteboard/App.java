23c3ae16bdd366b8a4f2ff53bee2760f692ce447
juan <juan@juans-air-2.fios-router.home>
2022-11-01 20:27:57 -0400

    Created all four views

--- src/main/java/com/mycompany/whiteboard/App.java
+++ src/main/java/com/mycompany/whiteboard/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("LoginScreen"), 640, 480);
+        scene = new Scene(loadFXML("login"), 640, 480);
+        stage.setTitle("Whiteboard");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/mycompany/whiteboard/LoginController.java
@@ -0,0 +1,23 @@
+package com.mycompany.whiteboard;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void switchToAdmin() throws IOException {
+        App.setRoot("admin");
+    }
+    
+    @FXML
+    private void switchToFaculty() throws IOException {
+        App.setRoot("faculty");
+    }
+    
+    @FXML
+    private void switchToStudent() throws IOException {
+        App.setRoot("student");
+    }
+    
+}
