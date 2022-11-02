0b994eaa0766f0f2129629714d50b1be6f2f7236
juan <juan@juans-air-2.fios-router.home>
2022-11-01 20:45:37 -0400

    Deleted unnecessary files

---
+++ src/main/java/com/mycompany/whiteboard/LoginController.java
@@ -0,0 +1,22 @@
+package com.mycompany.whiteboard;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void switchToAdmin() throws IOException {
+        App.setRoot("admin");
+    }
+    
+    @FXML
+    private void switchToFaculty() throws IOException {
+        App.setRoot("faculty");
+    }
+    
+    @FXML
+    private void switchToStudent() throws IOException {
+        App.setRoot("student");
+    }
+}
