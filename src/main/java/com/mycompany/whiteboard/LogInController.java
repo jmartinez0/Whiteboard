9608a7196e752ce1a4a6c99a8298e0e4714e1218
Juan <Juan@Juan>
2022-11-21 18:44:39 -0500

    Whiteboard Logo in login screen

--- src/main/java/com/mycompany/whiteboard/LogInController.java
+++ src/main/java/com/mycompany/whiteboard/LogInController.java
@@ -1,9 +1,23 @@
 package com.mycompany.whiteboard;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class LogInController {
+public class LogInController implements Initializable {
+    
+    @FXML ImageView whiteboardLogoImageView;
+    Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        whiteboardLogoImageView.setImage(whiteboardLogo);
+    }
+    
+    
    
 }
