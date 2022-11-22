6390f9a18def279211a7d9ce5e865f7ce9b5c76d
Juan <Juan@Juan>
2022-11-21 23:38:26 -0500

    Admin view rough draft

--- src/main/java/com/mycompany/whiteboard/AdminViewController.java
+++ src/main/java/com/mycompany/whiteboard/AdminViewController.java
@@ -1,5 +1,20 @@
 package com.mycompany.whiteboard;
 
-public class AdminViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class AdminViewController implements Initializable {
+    
+    @FXML private ImageView whiteboardLogoImageView;
+    Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        whiteboardLogoImageView.setImage(whiteboardLogo);
+    }
     
 }
