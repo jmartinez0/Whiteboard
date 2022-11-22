f087953cf1fcacf28460b9bb99bee12e97690499
Juan <Juan@Juan>
2022-11-21 19:51:03 -0500

    Login screen

--- src/main/java/com/mycompany/whiteboard/LogInController.java
+++ src/main/java/com/mycompany/whiteboard/LogInController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,10 +13,13 @@ public class LogInController implements Initializable {
     
     @FXML ImageView whiteboardLogoImageView;
     Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
+    @FXML private TextField usernameField, passwordField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         whiteboardLogoImageView.setImage(whiteboardLogo);
+        usernameField.setFocusTraversable(false);
+        passwordField.setFocusTraversable(false);
     }
     
     
