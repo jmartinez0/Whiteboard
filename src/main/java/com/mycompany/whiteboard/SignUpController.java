d68663d92024d7e4b6b8b4a4763ce1bd14c4d07f
Juan <Juan@Juan>
2022-11-21 20:23:30 -0500

    Added combo box to sign up screen

--- src/main/java/com/mycompany/whiteboard/SignUpController.java
+++ src/main/java/com/mycompany/whiteboard/SignUpController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -12,8 +13,9 @@ import javafx.scene.image.ImageView;
 
 public class SignUpController implements Initializable {
     
-    @FXML private TextField usernameField, nameField, emailField;
+    @FXML private TextField usernameField, nameField;
     @FXML private PasswordField passwordField;
+    @FXML private ComboBox typeOfUserComboBox;
     @FXML ImageView whiteboardLogoImageView;
     Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
         
@@ -22,13 +24,14 @@ public class SignUpController implements Initializable {
         usernameField.setFocusTraversable(false);
         passwordField.setFocusTraversable(false);
         nameField.setFocusTraversable(false);
-        emailField.setFocusTraversable(false);
+        String[] options = {"Student", "Faculty"}; 
+        typeOfUserComboBox.getItems().addAll(options);
+        typeOfUserComboBox.getSelectionModel().selectFirst();
     }
     
     @FXML public void switchToLogIn() throws IOException {
         App.setRoot("LogIn");
     }
-    
     
     
 }
