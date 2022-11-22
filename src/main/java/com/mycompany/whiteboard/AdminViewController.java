15a1873b32fbf576e8aee09beeb37b8fffd73e30
juan <juan@juans-air-2.fios-router.home>
2022-11-22 00:41:10 -0500

    Updated all three controllers and views with a common header

--- src/main/java/com/mycompany/whiteboard/AdminViewController.java
+++ src/main/java/com/mycompany/whiteboard/AdminViewController.java
@@ -1,20 +1,40 @@
 package com.mycompany.whiteboard;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class AdminViewController implements Initializable {
-    
-    @FXML private ImageView whiteboardLogoImageView;
-    Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @FXML private Label logOutLabel;
+    @FXML private ImageView whiteboardLogoImageView;
+    Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogoWhite.png"));
+
+    
+    @Override public void initialize(URL url, ResourceBundle rb) {
         whiteboardLogoImageView.setImage(whiteboardLogo);
     }
+
     
+    @FXML public void manageUsers() {
+
+    }
+
+    @FXML public void logOut() throws IOException {
+        Stage oldStage = (Stage) logOutLabel.getScene().getWindow();
+        oldStage.close();
+        Stage newStage = new Stage();
+        Scene scene = new Scene(App.loadFXML("LogIn"), 500, 500);
+        newStage.setTitle("Whiteboard");
+        newStage.setScene(scene);
+        newStage.show();
+    }
+
 }
