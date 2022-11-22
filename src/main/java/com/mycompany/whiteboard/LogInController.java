1eb5b7d4a33bb094b0bf7362642f6a6a2ea9a661
Juan <Juan@Juan>
2022-11-21 21:43:15 -0500

    Updated log in method to create different sized stages

--- src/main/java/com/mycompany/whiteboard/LogInController.java
+++ src/main/java/com/mycompany/whiteboard/LogInController.java
@@ -5,16 +5,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class LogInController implements Initializable {
     
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
-    @FXML ImageView whiteboardLogoImageView;
+    @FXML private Button logInButton;
+    @FXML private ImageView whiteboardLogoImageView;
     Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
 
     @Override public void initialize(URL url, ResourceBundle rb) {
@@ -27,17 +31,37 @@ public class LogInController implements Initializable {
         App.setRoot("SignUp");
     }
     
-    @FXML public void signIn() throws IOException {
+    @FXML public void logIn() throws IOException {
         if (usernameField.getText().equals("admin")) {
-            App.setRoot("AdminView");
+            Stage oldStage = (Stage) logInButton.getScene().getWindow();
+            oldStage.close();
+            Stage newStage = new Stage();
+            Scene scene = new Scene(App.loadFXML("AdminView"), 960, 600);
+            newStage.setTitle("Whiteboard");
+            newStage.setScene(scene);
+            newStage.show();  
         }
         if (usernameField.getText().equals("faculty")) {
-            App.setRoot("FacultyView");
+            Stage oldStage = (Stage) logInButton.getScene().getWindow();
+            oldStage.close();
+            Stage newStage = new Stage();
+            Scene scene = new Scene(App.loadFXML("FacultyView"), 960, 600);
+            newStage.setTitle("Whiteboard");
+            newStage.setScene(scene);
+            newStage.show();
         }
         if (usernameField.getText().equals("student")) {
-            App.setRoot("StudentView");
+            Stage oldStage = (Stage) logInButton.getScene().getWindow();
+            oldStage.close();
+            Stage newStage = new Stage();
+            Scene scene = new Scene(App.loadFXML("StudentView"), 960, 600);
+            newStage.setTitle("Whiteboard");
+            newStage.setScene(scene);
+            newStage.show();
         }
     }
+
+   
     
    
 }
