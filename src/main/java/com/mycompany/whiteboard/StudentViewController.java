ebb2c9df67ffaaf42007a912e2459714ac7a2fe8
cgonc <cgonc@DESKTOP-SAQKE1T.home>
2022-12-01 19:43:38 -0500

    Added method to StudentViewController to download docs

--- src/main/java/com/mycompany/whiteboard/StudentViewController.java
+++ src/main/java/com/mycompany/whiteboard/StudentViewController.java
@@ -2,7 +2,13 @@ package com.mycompany.whiteboard;
 
 import java.io.IOException;
 import java.net.URL;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
@@ -27,6 +33,36 @@ public class StudentViewController implements Initializable {
     @FXML
     public void courses() {
 
+    }
+    
+    @FXML
+    public void handleGetDoc() {
+
+    }
+
+    public static void getDoc(String projectId, String bucketName,
+            String objectName, String destFilePath) {
+
+        // The ID of your GCP project
+        projectId = "WhiteboardCSC325";
+        // The ID of your GCS bucket
+        bucketName = "whiteboardcsc325";
+        // The ID of your GCS object
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("input file name: ");
+        objectName = myObj.nextLine();
+        // The path to which the file should be downloaded
+        System.out.println("input where file should be downloaded:");
+        destFilePath = myObj.nextLine();
+
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+        Blob blob = storage.get(BlobId.of(bucketName, objectName));
+        blob.downloadTo(Paths.get(destFilePath));
+
+        System.out.println("Downloaded object " + objectName + " from bucket name " + bucketName + " to " + destFilePath);
+
+        myObj.close();
     }
 
     @FXML
