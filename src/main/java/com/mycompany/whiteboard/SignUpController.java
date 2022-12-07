c8e97637907590d9c81413d01df33208297f1733
Juan <Juan@Juan>
2022-12-06 20:20:42 -0500

    Changed JavaFX version to 19 + added a custom icon for the application

--- src/main/java/com/mycompany/whiteboard/SignUpController.java
+++ src/main/java/com/mycompany/whiteboard/SignUpController.java
@@ -1,8 +1,12 @@
 package com.mycompany.whiteboard;
 
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -12,26 +16,54 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class SignUpController implements Initializable {
-    
-    @FXML private TextField usernameField, nameField;
-    @FXML private PasswordField passwordField;
-    @FXML private ComboBox typeOfUserComboBox;
-    @FXML ImageView whiteboardLogoImageView;
+
+    @FXML
+    private TextField usernameField, nameField, emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private ComboBox typeOfUserComboBox;
+    @FXML
+    ImageView whiteboardLogoImageView;
     Image whiteboardLogo = new Image(getClass().getResourceAsStream("WhiteboardLogo.png"));
-        
-    @Override public void initialize(URL url, ResourceBundle rb) {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
         whiteboardLogoImageView.setImage(whiteboardLogo);
         usernameField.setFocusTraversable(false);
         passwordField.setFocusTraversable(false);
         nameField.setFocusTraversable(false);
-        String[] options = {"Student", "Faculty"}; 
+        emailField.setFocusTraversable(false);
+        String[] options = {"Student", "Faculty"};
         typeOfUserComboBox.getItems().addAll(options);
         typeOfUserComboBox.getSelectionModel().selectFirst();
     }
-    
-    @FXML public void switchToLogIn() throws IOException {
+
+    @FXML
+    public void switchToLogIn() throws IOException {
         App.setRoot("LogIn");
     }
-    
-    
+
+    public void signUp() {
+        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
+                .setUid(usernameField.getText())
+                .setPassword(passwordField.getText())
+                .setDisplayName(nameField.getText().trim())
+                .setEmail(emailField.getText().trim());
+
+        UserRecord userRecord;
+        try {
+            userRecord = App.fauth.createUser(request);
+            System.out.println("Successfully created new user: " + userRecord.getUid());
+        } catch (FirebaseAuthException ex) {
+            // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Could not create new user");
+        }
+        try {
+            App.setRoot("LogIn");
+        } catch (IOException ex) {
+            Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
